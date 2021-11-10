934ef40f037c0059ac45cc92c50167171fe5795c
sonyak <kch9141@naver.com>
2021-11-10 23:04:28 +0900

    아이템 줍기 - 사각형 겹친 좌표 구현
    그래프 상에서 어떻게 탐색하며 이동할 것인지 구현 하면 끝

--- 21.11.week2/picking_item_up.py
+++ 21.11.week2/picking_item_up.py
@@ -1,7 +1,23 @@
+import numpy as np
+import sys
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
-    maps = [[0 for j in range(50)] for i in range(50)]  # 걸어 다닐 수 있는 좌표들을 1 로 적을꺼양
-    return answer
+    maps = [[0 for j in range(11)] for i in range(11)]  # 걸어 다닐 수 있는 좌표들을 1 로 적을꺼양
+    for rec in rectangle: # 사각형의 최소꼭짓점과 최대꼭지점을 하나씩 빼서
+        rectangle_line = find_rectangle_coordinates(rec) # 사각형의 변들이 좌표로 되어잇음
+        for cord in rectangle_line:
+            x, y = cord # 좌표를 풀어
+            all_x, all_y = [rec[0], rec[2]], [rec[1], rec[3]]
+            if x in all_x and y in all_y: # 사각 형의 꼭짓점에 해당하는 좌표일 때 - 꼭짓점에서는 절대 만나지 않는다는 규칙이 잇으므로
+                maps[x][y] = 2 # 꼭짓점은 2로 표시한다
+            else: # 꼭짓점이 아닐때 -- 일반 선분이 위치하거나, 교차점이 발생한다.
+                if maps[x][y] == 1:
+                    maps[x][y] = 3 # 교차점은 3 으로 표시한다.
+                else:
+                    maps[x][y] = 1 # 일반 선분은 1 로 표시한다.
+
+
+    return maps
 
 
 # 꼭짓점이랑 교차점에서 이동방향을 바꾸는 식으로 플레이를 해봅시다.
@@ -18,11 +34,18 @@ def find_rectangle_coordinates(rectangle):  # 좌표뽑기 코드
             x += cord[0]
             y += cord[1]
             rec_cord.append((x, y))  # 여기서 꼭짓점 교찻점 표시할지, 맵에 표시할지 그런거 생각 ㄱ
+    return rec_cord # 좌표만 리턴 할게용
 
 
-# rec = [[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]]
-# cx, cy = 1, 3
-# ix, iy = 7, 8
+
+rec = [[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]]
+cx, cy = 1, 3
+ix, iy = 7, 8
+
+np.set_printoptions(threshold=sys.maxsize)
+print(np.array(solution(rec, cx, cy, ix, iy)))
+
+
 
 # minx, miny, maxx, maxy = [1,1,7,7]
 # count = [maxy - miny, maxx - minx]
