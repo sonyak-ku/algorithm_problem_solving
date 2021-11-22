8c62363bb345f59f26724ecdb5e9e38d12a75bd9
sonyak <kch9141@naver.com>
2021-11-22 12:11:04 +0900

    dict sorted 연습 - 리턴은 list  형태

--- 2021.nov.week4/prac.py
+++ 2021.nov.week4/prac.py
@@ -1,4 +1,8 @@
 
-able_to_start = [[1, 2]]
-able_to_start.sort(key=lambda x: x[1])
-print(able_to_start)
+# able_to_start = [[1, 2]]
+# able_to_start.sort(key=lambda x: x[1])
+# print(able_to_start)
+
+d = {'a': 1, 'b': 2, 'c': 3}
+a = sorted(d.items(), key=lambda x: x[1], reverse= True)
+print(a)
